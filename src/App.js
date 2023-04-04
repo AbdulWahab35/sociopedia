0e838b2786273b7606d119f0842db5a80bb22965
AbdulWahab35 <121030625+AbdulWahab35@users.noreply.github.com>
2023-04-04 07:49:38 -0700

    name changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with wahab
+          Learn React with Max
         </a>
       </header>
     </div>
